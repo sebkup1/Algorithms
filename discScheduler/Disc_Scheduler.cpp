6b6b3d0b81fdf67790e65fe5b89e73940908dc7b
sebkup1 <sebkup1@gmail.com>
2017-10-06 15:14:02 +0200

    adding disc scheduler

---
+++ discScheduler/Disc_Scheduler.cpp
@@ -0,0 +1,38 @@
+bool* myTrack;
+int curHead;
+
+void init(int track_size, int head){
+  myTrack = new bool[track_size];
+  curHead = head;
+}
+
+void request(int track){
+  if (myTrack[track])
+  {
+    new int;
+  }
+  else
+  {
+    new int;
+  }
+}
+
+int fcfs(){
+  int track_no = -1;	// TO DO : Need to be changed
+  return track_no;
+}
+
+int sstf(){
+  int track_no = -1;	// TO DO : Need to be changed
+  return track_no;
+}
+
+int look(){
+  int track_no = -1;	// TO DO : Need to be changed
+  return track_no;
+}
+
+int clook(){
+  int track_no = -1;	// TO DO : Need to be changed
+  return track_no;
+}
